a7f9565bb4b61a46caf5ffbd77e590b4e9a3f630
ZeeLugalo <4271363@myuwc.ac.za>
2022-08-02 13:03:24 +0200

    Add project files.

---
+++ Exercise 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            Console.WriteLine("Enter the 5 numbers");
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+                double num = Convert.ToDouble(Console.ReadLine());
+                sum += num;
+            }
+            double average = sum / 5;
+            Console.WriteLine( $"The Average is:{average}");
+        }
+    }
+}
